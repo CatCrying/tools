344b496de8f3b65e16fa494cf4e1fd8a56d172b5
CatCrying <88854387+CatCrying@users.noreply.github.com>
2025-10-29 21:50:23 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite';
+import javascriptObfuscator from './plugins/obfuscator.mjs';
+
 
 export default defineConfig({
+  build: {
+    sourcemap: false,
+  },
+    plugins: [
+    javascriptObfuscator({
+      include: ["**/*.js"],
+      options: {
+
+
+        
+      }
+    }),
+  ],
   define: {
     // This injects the build timestamp into our JavaScript
     __BUILD_TIMESTAMP__: JSON.stringify(new Date().toISOString()),
